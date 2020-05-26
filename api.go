5ffadee5da3774213d512efe92ecf2abc6861ed8
Kirill Kolesnikov <kkxnes@gmail.com>
2020-05-26 12:40:07 +0300

    Update README.md

--- api.go
+++ api.go
@@ -1,4 +1,4 @@
-// Package mixer contains the HTTP multiplexer based on similar trie data structure.
+// Package mixer contains the HTTP multiplexer based on similar radix tree data structure.
 package mixer
 
 import (
